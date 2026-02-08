8017dac6f1a40f29621d1fe9a9c0fa6e764c9f87
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-02-08 05:23:34 -0300

    initial structure

---
+++ infrastructure/src/main/java/com/fiap/StockServiceApplication.java
@@ -0,0 +1,21 @@
+package com.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+
+public class StockServiceApplication
+{
+    /*public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }*/
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApplication.class, args);
+    }
+}
