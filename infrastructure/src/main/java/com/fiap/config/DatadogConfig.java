60d66274f1ab99aab62fef2b6756244ea3a5a014
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-02-21 01:29:21 -0300

    remove datadog

--- infrastructure/src/main/java/com/fiap/config/DatadogConfig.java
+++ infrastructure/src/main/java/com/fiap/config/DatadogConfig.java
@@ -2,9 +2,11 @@ package com.fiap.config;
 
 import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
 import com.timgroup.statsd.StatsDClient;
+import com.timgroup.statsd.StatsDClientException;
 import datadog.trace.api.GlobalTracer;
 import datadog.trace.api.Tracer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,11 +25,18 @@ public class DatadogConfig {
     }
 
     @Bean
+    @ConditionalOnProperty(name = "datadog.enabled", havingValue = "true", matchIfMissing = false)
     public StatsDClient statsDClient() {
-        return new NonBlockingStatsDClientBuilder()
-                .prefix("stock")
-                .hostname(statsdHost)
-                .port(statsdPort)
-                .build();
+        try {
+            return new NonBlockingStatsDClientBuilder()
+                    .prefix("stock")
+                    .hostname(statsdHost)
+                    .port(statsdPort)
+                    .build();
+        } catch (StatsDClientException e) {
+            // Log warning but don't fail application startup
+            System.err.println("Warning: Failed to create StatsD client: " + e.getMessage());
+            return null;
+        }
     }
 }
