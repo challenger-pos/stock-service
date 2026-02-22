4a98836ed7f242950e2a94e66a5ea5d898f93cec
Luigi Gevaerd Braghittoni <luigi.gbraghittoni@gmail.com>
2026-02-22 16:37:23 -0300

    stored key

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-challenge-bucket"
     region         = "us-east-2"
+    # Será passado dinamicamente na pipe = "v4/stock-service/dev/terraform.tfstate" 
   }
 }
